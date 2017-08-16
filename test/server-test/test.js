476b4eef838a7fa08cb26941576ca2b88c6300b5
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2017-08-15 22:32:23 -0300

    Now basic server getting the desired info with appropriate tests.

--- test/server-test/test.js
+++ test/server-test/test.js
@@ -1,25 +1,28 @@
 var request = require('supertest');
 var app = require('../../app.js');
+var expect = require('chai').expect
 
 describe('GET /all-vehicles', function(){
 
   it('Should Respond With A 200 Status Code', function(done){
     request(app)
       .get('/all-vehicles')
-      .expect(200) // note that we're passing the done as parameter to the expect
-      .end(function(err, res) {
-        if (err) return done(err);
-        done();
-      });
+      .expect(200,done) // note that we're passing the done as parameter to the expect
   });
 
   it('Should Return JSON', function(done) {
     request(app)
       .get('/all-vehicles')
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        done();
-      });
+      .expect('Content-Type', /json/, done)
+  })
+
+  it('Should Be An Array of Latitudes And longitudes', function(done){
+    request(app)
+      .get('/all-vehicles')
+      .then( res => {
+        expect(res.body[0].lat).to.not.equal(null)
+        expect(res.body[0].lon).to.not.equal(null)
+      })
+      .then(done)
   })
 });
