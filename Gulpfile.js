a9d0a58e5991125ca9e10f4e6b64b4c4246f9756
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2017-08-18 21:11:55 -0300

    Started to transform jsx to js.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,16 +3,19 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
+var bableify = require('babelify')
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function(){
   var b = browserify({
-    entries: 'index.js',
+    entries: 'index.jsx',
   });
 
-  return b.bundle()
-   .pipe(source('index.js'))
-   .pipe(buffer())
-   .pipe(uglify())
-   .on("error", gutil.log)
-   .pipe(gulp.dest('dist/'))
+  return b.transform("babelify", {presets: ["es2015", "react"]})
+    .bundle()
+    .pipe(source('index.jsx'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .on("error", gutil.log)
+    .pipe(gulp.dest('dist/'))
 });
